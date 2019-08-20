13e2430a55cbd66daa99771636c2bb429109db75
Yannick DJOPGANG <nzeneyannick@yahoo.fr>
2019-08-20 20:04:20 +0200

    creation du module routing, creation du service catalogue et affichage des information de categories depuis le backend java

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, RouterModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
